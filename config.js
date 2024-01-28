1ff208c73f2c5ec88a2f91b5fc0aff41a162d1a3
JohnJohnMohn <96588320+JohnJohnMohn@users.noreply.github.com>
2024-01-28 14:04:27 -0500

    config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
 	name: 'Euneke',
 	imageBackground: false,
 	openInNewTab: true,
-	twelveHourFormat: false,
+	twelveHourFormat: true,
 
 	// Greetings
 	greetingMorning: 'Good morning!',
